0511204899ff36842b758587cc6a5a4be54237a8
DavidCbd <davidcbd17@gmail.com>
2024-09-17 13:06:25 -0600

    Modificaciones a servicios

--- src/app/screens/login-screen/login-screen.component.ts
+++ src/app/screens/login-screen/login-screen.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FacadeService } from '../../services/facade.service';
+import { FacadeService } from '../../../services/facade.service';
 
 @Component({
   selector: 'app-login-screen',
