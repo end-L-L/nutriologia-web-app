32f18c3d73a89f9637635e353321fc210696bb33
Jared Muñoz <amunozbaez669@gmail.com>
2024-10-26 09:05:01 -0600

    fix: update components imports

--- src/app/patients/patients.module.ts
+++ src/app/patients/patients.module.ts
@@ -3,9 +3,9 @@ import { CommonModule } from '@angular/common';
 import { PatientsRoutingModule } from './patients-routing.module';
 import { MainComponent } from './pages/main/main.component';
 
-import { ProgressBarComponent } from './components/progress-bar';
-import { ButtonComponent } from './components/button';
-import { InputComponent, InputContainerComponent, InputLabelComponent } from './components/input';
+import { ProgressBarComponent } from './components/ui/progress-bar';
+import { ButtonComponent } from './components/ui/button';
+import { InputComponent, InputContainerComponent, InputLabelComponent } from './components/ui/input';
 
 @NgModule({
   declarations: [MainComponent, ProgressBarComponent, InputComponent, InputLabelComponent, InputContainerComponent, ButtonComponent],
