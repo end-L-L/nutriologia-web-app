f7ee5a4426fdd0ffedd4abf8d2a53c5d5aca1ea3
Jared Muñoz <amunozbaez669@gmail.com>
2024-10-26 08:22:31 -0600

    feat: add macro interfaces

---
+++ src/app/patients/interfaces/macros.interface.d.ts
@@ -0,0 +1,10 @@
+export interface Macros {
+  calorias: number;
+  caloriasMax: number;
+  proteinas: number;
+  proteinasMax: number;
+  carbohidratos: number;
+  carbohidratosMax: number;
+  grasas: number;
+  grasasMax: number;
+}
