ba5d2b70b5cb29478df80e313e16be5e4208fc23
Jared Muñoz <amunozbaez669@gmail.com>
2024-10-25 20:28:22 -0600

    feat: add patients routes and format code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';  // Requerido por Angular Material
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Requerido por Angular Material
 
 //Este import es para los servicios HTTP
 import { HttpClientModule } from '@angular/common/http';
 
 // Importaciones de Angular Material
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatNativeDateModule} from '@angular/material/core';
-import {MatTableModule} from '@angular/material/table';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatSelectModule} from '@angular/material/select';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-
+import { MatRadioModule } from '@angular/material/radio';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 // Otros componentes
 import { LoginScreenComponent } from './screens/login-screen/login-screen.component';
@@ -31,7 +30,6 @@ import { RegistroAdminComponent } from './partials/registro-admin/registro-admin
 import { RegistroNutriologoComponent } from './partials/registro-nutriologo/registro-nutriologo.component';
 import { RegistroPacienteComponent } from './partials/registro-paciente/registro-paciente.component';
 
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -41,14 +39,13 @@ import { RegistroPacienteComponent } from './partials/registro-paciente/registro
     RegistroAdminComponent,
     RegistroNutriologoComponent,
     RegistroPacienteComponent,
-
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,  // Necesario para Angular Material
-    MatFormFieldModule,       // Módulo de formularios de Angular Material
-    MatInputModule,           // Módulo de inputs de Angular Material
+    BrowserAnimationsModule, // Necesario para Angular Material
+    MatFormFieldModule, // Módulo de formularios de Angular Material
+    MatInputModule, // Módulo de inputs de Angular Material
     MatDialogModule, // Importa MatDialogModule aquí
     MatIconModule, //para los iconos
     MatButtonModule,
@@ -62,12 +59,8 @@ import { RegistroPacienteComponent } from './partials/registro-paciente/registro
     MatNativeDateModule,
     MatTableModule,
     MatPaginatorModule,
-
-
-
-
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
