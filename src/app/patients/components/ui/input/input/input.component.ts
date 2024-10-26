8755ae885c0a423258032956e548e8869fa62446
Jared Muñoz <amunozbaez669@gmail.com>
2024-10-26 09:53:19 -0600

    feat: style input component

--- src/app/patients/components/ui/input/input/input.component.ts
+++ src/app/patients/components/ui/input/input/input.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'input-component',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss'],
 })
-export class InputComponent {}
+export class InputComponent {
+  @Input() name: string = '';
+  @Input() placeholder: string = '';
+  @Input() type: 'text' | 'number' | 'email' | 'password' = 'text';
+  @Input() value: string | number = '';
+  @Input() id: string = '';
+}
