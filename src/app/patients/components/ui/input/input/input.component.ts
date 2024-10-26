8d6e080e12dbc2b077ddab34df0f2485ccd05756
Jared Muñoz <amunozbaez669@gmail.com>
2024-10-26 09:04:25 -0600

    fix: move ui components from components to components/ui

---
+++ src/app/patients/components/ui/input/input/input.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'input-component',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss'],
+})
+export class InputComponent {}
