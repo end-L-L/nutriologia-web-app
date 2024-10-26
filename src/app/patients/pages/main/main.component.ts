9f8754bb913a1e5f3f57597b2e927767ed989f60
Jared Muñoz <amunozbaez669@gmail.com>
2024-10-25 20:27:30 -0600

    feat: set initial folder structure

---
+++ src/app/patients/pages/main/main.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-page',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss'],
+})
+export class MainComponent {}
---
+++ src/app/patients/patients.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PatientsRoutingModule } from './patients-routing.module';
+import { MainComponent } from './pages/main/main.component';
+
+@NgModule({
+  declarations: [
+    MainComponent
+  ],
+  imports: [CommonModule, PatientsRoutingModule],
+})
+export class PatientsModule {}
