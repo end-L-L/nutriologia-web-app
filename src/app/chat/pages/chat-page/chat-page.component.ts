02a281d76821a31878181959f492b4adc4063b52
Jared Muñoz <amunozbaez669@gmail.com>
2024-11-30 16:52:54 -0600

    fix: rename chat page selector

--- src/app/chat/pages/chat-page/chat-page.component.ts
+++ src/app/chat/pages/chat-page/chat-page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-chat-page',
+  selector: 'chat-page',
   templateUrl: './chat-page.component.html',
   styleUrls: ['./chat-page.component.scss'],
 })
