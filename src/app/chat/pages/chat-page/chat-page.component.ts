0956664e76ebd1cccbd0d0921239b03833509011
Jared Muñoz <amunozbaez669@gmail.com>
2024-11-30 18:55:45 -0600

    feat: internal chat working with the serviceChat Service

--- src/app/chat/pages/chat-page/chat-page.component.ts
+++ src/app/chat/pages/chat-page/chat-page.component.ts
@@ -1,30 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { io } from 'socket.io-client';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'chat-page',
   templateUrl: './chat-page.component.html',
   styleUrls: ['./chat-page.component.scss'],
 })
-export class ChatPageComponent {
-  currentDate = new Date();
-  messages = [
-    {
-      text: '¡Hola! 👋 Bienvenido a tu sesión de nutrición. ¿Cómo puedo ayudarte hoy?',
-      sender: 'bot',
-    },
-    {
-      text: 'Hola, me gustaría obtener consejos para una dieta balanceada 😊',
-      sender: 'user',
-    },
-    {
-      text: 'Claro, estaré encantado de ayudarte. Para empezar, ¿podrías decirme cuáles son tus objetivos principales? 🎯',
-      sender: 'bot',
-    },
-  ];
+export class ChatPageComponent implements OnInit {
+  private socket = io(environment.CHAT_URL);
+  private usersConnected: string[] = [];
 
-  sendMessage(input: HTMLInputElement): void {
+  public currentDate = new Date();
+  public messages: { text: string; ownerId: string; receptorId: string; date: Date; id: string }[] = [];
+  public myId = window.localStorage.getItem('userId');
+
+  ngOnInit(): void {
+    this.socket.emit('join', {
+      userId: window.localStorage.getItem('userId'),
+      otherUserId: window.localStorage.getItem('otherUserId'),
+    });
+
+    this.socket.emit('users');
+
+    this.socket.on('join', (data) => {
+      this.messages = data.messages;
+    });
+
+    this.socket.on('users', (data) => {
+      this.usersConnected = data.users;
+    });
+
+    this.socket.on('message', (data) => {
+      const [message] = data;
+      this.messages.push(message);
+    });
+  }
+
+  public isConnected() {
+    return this.socket.connected;
+  }
+
+  public isOtherUserConnected() {
+    const otherUser = window.localStorage.getItem('otherUserId') ?? '';
+    return this.usersConnected.includes(otherUser);
+  }
+
+  public getUsersConnected() {
+    return this.usersConnected;
+  }
+
+  public sendMessage(e: SubmitEvent, input: HTMLInputElement): void {
+    e.preventDefault();
     if (input.value.trim()) {
-      this.messages.push({ text: input.value, sender: 'user' });
+      this.socket.emit('message', {
+        userId: window.localStorage.getItem('userId'),
+        receptorId: window.localStorage.getItem('otherUserId'),
+        message: input.value,
+      });
       input.value = '';
     }
   }
